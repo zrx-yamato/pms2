edc0f288342af027459bf6556bf9f5534c00cbf4
syuheiochiai <syuhei.ochiai@kana-me.com>
2021-03-03 22:09:29 +0900

    ダッシュボードページの追加

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Dashboard Controller
+ */
+class DashboardController extends AuthController
+{
+
+    /**
+     * Index method
+     */
+    public function index()
+    {
+
+    }
+}
